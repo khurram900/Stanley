777bc7b0bde59b5693a6bd2607e23d591436b550
Khurram <mkhurram900@gmail.com>
2019-08-31 04:57:27 +0500

    Admin middleware added

---
+++ src/Http/Middleware/AdminAuthentication.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace StanleyMSACommon\MSACommon\Http\Middleware;
+
+use App\Services\AuthService;
+use Closure;
+use Auth;
+use StanleyMSACommon\MSACommon\Common\ApiResponseCodesBook;
+use StanleyMSACommon\MSACommon\Exceptions\APIException;
+use StanleyMSACommon\MSACommon\Services\AuthUserService;
+use View;
+
+class AdminAuthentication
+{
+    /* @var $authUserService AuthUserService*/
+    private $authUserService;
+
+    public function __construct(AuthUserService $authUserService){
+        $this->authUserService = $authUserService;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!request()->header('token')) {
+            throw new APIException(ApiResponseCodesBook::NOT_LOGGED_IN);
+        }
+
+        $user = $this->authUserService->getUser();
+
+        if (!$user['type'] == 'ADMIN') {
+            throw new APIException(ApiResponseCodesBook::ADMIN_ACCESS_ONLY);
+        }
+
+        $request->request->add([
+            'user' => $user
+        ]);
+
+        return $next($request);
+    }
+}
